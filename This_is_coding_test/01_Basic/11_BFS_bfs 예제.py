0b5f4fc418b3f0079092a7e3ceee77d6edc17431
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-04-29 18:54:57 +0900

    solve: BFS 예제풀이 (147p)

---
+++ This_is_coding_test/01_Basic/11_BFS_bfs 예제.py
@@ -0,0 +1,40 @@
+# [11] BFS - bfs 예제
+
+
+from collections import deque
+
+# 1. BFS 메서드 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복 = 모든 노드를 방문할 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i]  = True
+
+# 2. 노드 연결 정보 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 3. 노드 방문 정보 표현(1차원 리스트)
+visited = [False] * 9
+
+# 4. BFS 함수 호출
+bfs(graph, 1, visited)
