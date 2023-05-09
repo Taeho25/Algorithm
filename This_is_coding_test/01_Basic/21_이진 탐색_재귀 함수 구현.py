f548ab77597ccd824dac445e96c5187abd9932f7
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-05-09 23:16:26 +0900

    solve: 이진 탐색 예제풀이 (189p)

---
+++ This_is_coding_test/01_Basic/21_이진 탐색_재귀 함수 구현.py
@@ -0,0 +1,28 @@
+# [21] 이진 탐색 - 재귀함수 구현
+
+
+# 1. 이진 탐색 구현(재귀함수)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    # 찾은 경우 중간점 인덱스 반환
+    if array[mid] == target:
+        return mid
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우 왼쪽 확인
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+# 2. N(원소의 개수), target(찾고자 하는 문자열) / array(전체 원소) 입력
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+# 3. 결과 출력
+result = binary_search(array, target, 0, n-1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
