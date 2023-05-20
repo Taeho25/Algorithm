2c2cd4b354f3b4ca571913219df3001b2ed1d8af
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-05-20 22:56:47 +0900

    solve: 연습문제 문제풀이

--- Programmers/Lv.2/멀리뛰기.py
+++ Programmers/Lv.2/멀리뛰기.py
@@ -1,24 +1,25 @@
-'''Lv.2 피보나치 수'''
+'''Lv.2 멀리뛰기'''
 
-# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/12945
+# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/12914
 
 
 # 풀이 1
 def solution(n):
-    f = [0]*100001
-    f[0], f[1] = 0, 1
+    step = [0]*2001
+    step[1] = 1
+    step[2] = 2
     
-    for num in range(2, n+1):
-        f[num] = f[num-1] + f[num-2]
+    for i in range(3, n+1):
+        step[i] = step[i-1] + step[i-2] 
     
-    return f[n] % 1234567
+    return step[n] % 1234567
 
 
 # 풀이 2
 def solution(n):
-    a, b = 0, 1
+    a, b = 1, 2
     
-    for _ in range(n):
-        a, b = b, a + b
-        
+    for _ in range(n-1):
+        a, b = b, a + b 
+    
     return a % 1234567
