a1e2afa59ce894311ea7e8f663bdff97c85aab96
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-05-21 13:16:09 +0900

    solve: 코딩테스트 입문 문제풀이

---
+++ Programmers/Lv.0/옹알이 (1).py
@@ -0,0 +1,32 @@
+'''Lv.2 옹알이 (1)'''
+
+# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/120956
+
+
+# 풀이 1
+def solution(babbling):
+    answer = 0
+    joca_words = ["aya", "ye", "woo", "ma"]
+    
+    for word in babbling:
+        for joca_word in joca_words:
+            word = word.replace(joca_word, "_")
+        word = word.replace("_", "")
+        
+        if word == "":
+            answer += 1
+
+    return answer
+
+
+# 풀이 2
+import re
+def solution(babbling):
+    answer = 0
+    joca_words = re.compile('^(aya|ye|woo|ma)+$')
+    
+    for word in babbling:
+        if joca_words.match(word):
+            answer += 1
+
+    return answer
