4f67e255a3f23da2f7f5c38ff909f69a56cba25e
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-05-23 00:54:43 +0900

    solve: 최단 경로 예제풀이 (248p)

---
+++ This_is_coding_test/01_Basic/32_최단 경로_개선된 다익스트라 알고리즘.py
@@ -0,0 +1,55 @@
+# [32] 최단 경로 - 개선된 다익스트라 알고리즘
+
+
+# 풀이
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9)  # 무한을 의미하는 값(10억)
+
+# 노드 개수 N, 간선 개수 M 입력
+n, m = map(int, input().split())
+# 시작 노드 번호 입력
+start = int(input())
+# 각 노드에 연결되어있는 노드에 대한 정보를 담는 리스트 만들기
+graph = [[] for i in range(n+1)]
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보 입력
+for _ in range(m):
+    # a번 노드에서 b번 노드로 가는 거리가 c라는 의미
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+# 다익스트라 함수
+def dijkstra(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:  # 큐가 비어있지 않다면
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 형재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
