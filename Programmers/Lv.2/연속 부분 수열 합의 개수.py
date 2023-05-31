4e653ccedb2ebaa34e7ac749b66c5626b8eb7086
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-06-01 00:23:18 +0900

    solve: 연습문제 문제풀이

--- Programmers/Lv.2/연속 부분 수열 합의 개수.py
+++ Programmers/Lv.2/연속 부분 수열 합의 개수.py
@@ -5,12 +5,58 @@
 
 # 풀이 1
 def solution(elements):
+    ele_len = len(elements)
     answer = set()
-    elements2 = elements*2
-    for i in range(1, len(elements)):
-        for s in range(len(elements)):
+    elements = elements * 2
+
+    for i in range(1, ele_len + 1):
+        for s in range(ele_len):
             #print(elements2[s:s+i])
-            temp = sum(elements2[s:s+i])
-            answer.add(temp)
+            ssum = sum(elements[s:s+i])  # 이중for문 + sum() -> O(N^3)
+            answer.add(ssum)
         
-    return len(answer)+1
+    return len(answer)
+
+
+# 풀이 2
+def solution(elements):
+    ele_len = len(elements)
+    answer = set(elements)  # 부분수열 길이가 1인 경우의 합으로 초기화
+    elements = elements * 2
+
+    for s in range(2, ele_len):
+        for i in range(ele_len):
+            ssum = sum(elements[s:s+i])  # 이중for문 + sum() -> O(N^3)
+            answer.add(ssum)
+
+    return len(answer) + 1
+
+
+# 풀이 3
+def solution(elements):
+    ele_len = len(elements)
+    answer = set()
+
+    for s in range(ele_len):
+        ssum = elements[s]
+        answer.add(ssum)
+        for i in range(s+1, s+ele_len):
+            ssum += elements[i % ele_len]  # 이중for문 + ssum 사용 -> O(N^2)
+            answer.add(ssum)
+            
+    return len(answer)
+
+
+# 풀이 4
+def solution(elements):
+    ele_len = len(elements)
+    answer = set()
+
+    for s in range(ele_len):
+        ssum = elements[s]
+        answer.add(ssum)
+        for i in range(s+1, s+ele_len-1):
+            ssum += elements[i % ele_len]
+            answer.add(ssum)
+            
+    return len(answer) + 1
