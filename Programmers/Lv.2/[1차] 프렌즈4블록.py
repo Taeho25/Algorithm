2f4cb74db4a6fbd18f1f573d33ca168d8a0fbf65
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-07-04 23:41:13 +0900

    solve: 2018 KAKAO BLIND RECRUITMENT 문제풀이

---
+++ Programmers/Lv.2/[1차] 프렌즈4블록.py
@@ -0,0 +1,47 @@
+'''Lv.2 [1차] 프렌즈4블록'''
+
+# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/17679
+
+
+# 풀이 1
+
+
+
+# 풀이 2
+def solution(m, n, board):
+    for i in range(m):
+        board[i] = list(board[i])
+    
+    cnt = 0
+    rm = set()
+    while True:
+        # 보드를 순회하며 4블록이 된 곳의 좌표를 집합에 기록
+        for i in range(m-1):
+            for j in range(n-1):
+                t = board[i][j]
+                if t == []:
+                    continue
+                if board[i+1][j] == t and board[i][j+1] == t and board[i+1][j+1] == t:
+                    rm.add((i,j));rm.add((i+1,j))
+                    rm.add((i,j+1));rm.add((i+1,j+1))
+        
+        # 좌표가 존재한다면 집합의 길이만큼 세주고 블록을 지움 
+        if rm:
+            cnt += len(rm)
+            for i,j in rm:
+                board[i][j] = []
+            rm = set()
+        else:
+            return cnt
+        
+        # 블록을 위에서 아래로 당겨줌
+        while True:
+            moved = 0
+            for i in range(m-1):
+                for j in range(n):
+                    if board[i][j] and board[i+1][j]==[]:
+                        board[i+1][j] = board[i][j]
+                        board[i][j] = []
+                        moved = 1
+            if moved == 0:
+                break
