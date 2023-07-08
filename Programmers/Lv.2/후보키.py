810f9116521f103582673d30d2083863df5fc8e1
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-07-08 23:57:38 +0900

    solve: 2019 KAKAO BLIND RECRUITMENT 문제풀이

---
+++ Programmers/Lv.2/후보키.py
@@ -0,0 +1,26 @@
+'''Lv.2 후보키'''
+
+# 문제 : https://school.programmers.co.kr/learn/courses/30/lessons/42890
+
+
+# 풀이 1
+def solution(relation):
+    answer_list = list()
+    for i in range(1, 1 << len(relation[0])):
+        tmp_set = set()
+        for j in range(len(relation)):
+            tmp = ''
+            for k in range(len(relation[0])):
+                if i & (1 << k):
+                    tmp += str(relation[j][k])
+            tmp_set.add(tmp)
+
+        if len(tmp_set) == len(relation):
+            not_duplicate = True
+            for num in answer_list:
+                if (num & i) == num:
+                    not_duplicate = False
+                    break
+            if not_duplicate:
+                answer_list.append(i)
+    return len(answer_list)
