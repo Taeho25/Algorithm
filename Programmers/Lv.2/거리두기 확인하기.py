5da1b9ac6a0a700587e7ce65a16fd8471d0b6c88
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-07-16 00:53:30 +0900

    solve: 2021 카카오 채용연계형 인턴십 문제풀이

--- Programmers/Lv.2/거리두기 확인하기.py
+++ Programmers/Lv.2/거리두기 확인하기.py
@@ -48,7 +48,8 @@ def solution(places):
                                     good = 0
                                     break
         
-        answer.append(good) 
+        answer.append(good)
+            
     
     return answer
 
@@ -101,3 +102,27 @@ def solution(places):
         answer.append(good)
 
     return answer
+
+
+# 풀이 3
+def check(place):
+    for irow, row in enumerate(place):
+        for icol, cell in enumerate(row):
+            if cell != 'P':
+                continue
+            if irow != 4 and place[irow + 1][icol] == 'P':
+                return 0
+            if icol != 4 and place[irow][icol + 1] == 'P':
+                return 0
+            if irow < 3 and place[irow + 2][icol] == 'P' and place[irow + 1][icol] != 'X':
+                return 0
+            if icol < 3 and place[irow][icol + 2] == 'P' and place[irow][icol + 1] != 'X':
+                return 0
+            if irow != 4 and icol != 4 and place[irow + 1][icol + 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol + 1] != 'X'):
+                return 0
+            if irow != 4 and icol != 0 and place[irow + 1][icol - 1] == 'P' and (place[irow + 1][icol] != 'X' or place[irow][icol - 1] != 'X'):
+                return 0
+    return 1
+
+def solution(places):
+    return [check(place) for place in places]
