02390c1bb2760f6c871005233903657297e71168
Taeho Jung <76832875+Taeho25@users.noreply.github.com>
2023-07-16 00:46:43 +0900

    solve: 2021 카카오 채용연계형 인턴십 문제풀이

--- Programmers/Lv.2/거리두기 확인하기.py
+++ Programmers/Lv.2/거리두기 확인하기.py
@@ -4,6 +4,56 @@
 
 
 # 풀이 1
+def solution(places):
+    answer = []
+    dx = [-1, 1, 0, 0] # 상하좌우
+    dy = [0, 0, -1, 1]
+    places_list = [[list(p) for p in place] for place in places]
+    
+    # place 하나씩 확인
+    for place in places_list:
+        good = 1
+        
+        # place 내 5x5 공간 하나씩 확인
+        for x in range(5):
+            if good == 0: break
+            for y in range(5):
+                if good == 0: break
+                if place[x][y] != 'P': continue
+                
+                # 1. (x,y) 기준 방문처리 리셋
+                visited = [[False]*5 for _ in range(5)]
+                visited[x][y] = True
+                
+                # 2. Step 1
+                for i in range(4):
+                    if good == 0: break
+                    nx = x + dx[i]
+                    ny = y + dy[i]
+                    if (0 <= nx < 5) and (0 <= ny < 5) and visited[nx][ny] == False:
+                        visited[nx][ny] = True
+                        if place[nx][ny] == 'P':  # 사람이 있으면 거리두기 X
+                            good = 0
+                            break
+                        elif place[nx][ny] == 'X':  # 파티션 있으면 거리두기 O (더 안 봐도 됨)
+                            continue
+                        
+                        # 3. Step 2 (이전이 빈테이블인 경우만 한 번 더 가보기)
+                        for i2 in range(4):
+                            nx2 = nx + dx[i2]
+                            ny2 = ny + dy[i2]
+                            if (0 <= nx2 < 5) and (0 <= ny2 < 5) and visited[nx2][ny2] == False:
+                                visited[nx2][ny2] = True
+                                if place[nx2][ny2] == 'P':  # step1: 빈테이블 / step2: 사람 -> 거리두기 X
+                                    good = 0
+                                    break
+        
+        answer.append(good) 
+    
+    return answer
+
+
+# 풀이 2
 from collections import deque
 
 dx = [-1, 1, 0, 0]  # 상하좌우
